ace943f9e6964094a85e81e542e4abf596fb6f8f
Safeer <safer@aethondigital.com>
2025-08-07 09:04:41 +0500

    vdc dashoboard integrate in laravel

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,171 @@
+   <aside class="sidebar-wrapper" data-simplebar="true">
+          <div class="sidebar-header">
+            <div class="">
+             <!-- <h4>Fleet</h4> -->
+            </div>
+            <div>
+              <h4 class="logo-text"> <img src="{{asset('adminui/assets/images/avatars/fleetlogo.png')}}" class="logo-icon" alt="logo icon"></h4>
+            </div>
+            <div class="toggle-icon ms-auto"><i class="bi bi-chevron-double-left"></i>
+            </div>
+          </div>
+          <!--navigation-->
+          <ul class="metismenu" id="menu">
+            <li>
+              <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bi bi-grid"></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+              </a>
+              <!-- <ul>
+                <li> <a href="index.html"><i class="bi bi-arrow-right-short"></i>eCommerce</a>
+                </li>
+               
+              </ul> -->
+            </li>
+          
+              
+           
+           
+            <li>
+              <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bi bi-wrench-adjustable-circle"></i>
+                </div>
+                <div class="menu-title">Vehicle Checks</div>
+              </a>
+              <ul>
+                <li> <a href="widgets-static-widgets.html"><i class="bi bi-arrow-right-short"></i>Static Widgets</a>
+                </li>
+               
+              </ul>
+            </li>
+            <li>
+              <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bi bi-car-front"></i>
+                </div>
+                <div class="menu-title">Vehicles</div>
+              </a>
+              <ul>
+                <li> <a href="ecommerce-products-list.html"><i class="bi bi-arrow-right-short"></i>Products List</a>
+                </li>
+                <li> <a href="ecommerce-products-grid.html"><i class="bi bi-arrow-right-short"></i>Products Grid</a>
+                </li>
+              
+              </ul>
+            </li>
+            <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-person"></i>
+                </div>
+                <div class="menu-title">Drivers</div>
+              </a>
+              <ul>
+                <li> <a href="component-alerts.html"><i class="bi bi-arrow-right-short"></i>Alerts</a>
+                </li>
+                <li> <a href="component-accordions.html"><i class="bi bi-arrow-right-short"></i>Accordions</a>
+                </li>
+               
+              </ul>
+            </li>
+            <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-gear-wide-connected"></i>
+                </div>
+                <div class="menu-title">Defects</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+
+
+            <!-- <li class="sidebar-divider"></li> -->
+
+             <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-house"></i>
+                </div>
+                <div class="menu-title">Sites</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+             <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-exclamation-triangle"></i>
+                </div>
+                <div class="menu-title">Incidents</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+             <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-receipt"></i>
+                </div>
+                <div class="menu-title">Report</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+
+
+             <!-- <li class="sidebar-divider"></li> -->
+
+             <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-gear"></i>
+                </div>
+                <div class="menu-title">Setting</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+             <li>
+              <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-question-circle"></i>
+                </div>
+                <div class="menu-title">Help</div>
+              </a>
+              <ul>
+                <li> <a href="icons-line-icons.html"><i class="bi bi-arrow-right-short"></i>Line Icons</a>
+                </li>
+                <li> <a href="icons-boxicons.html"><i class="bi bi-arrow-right-short"></i>Boxicons</a>
+                </li>
+                <li> <a href="icons-feather-icons.html"><i class="bi bi-arrow-right-short"></i>Feather Icons</a>
+                </li>
+              </ul>
+            </li>
+           
+          </ul>
+          <!--end navigation-->
+       </aside>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+    Route::get('/admin/home', [AdminDashboardController::class, 'index'])->name('admin.index');
